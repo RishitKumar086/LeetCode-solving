257cf1e6dd0f1f3eb804577f213368fdb04230d6
RishitKumar086 <106433200+RishitKumar086@users.noreply.github.com>
2023-09-22 18:46:53 +0530

    Time: 3 ms (49.1%), Space: 6.8 MB (12.75%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(i<s.size() && j<t.size())
+        {
+            if(t[j]==s[i])
+                i++;
+            j++;
+        }
+        if(i==s.size())
+            return true;
+        return false;
+    }
+};
